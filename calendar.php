ca79e83e691990f92e5bd2cf5a0400f39dc64afb
Samulikostamo <Samulikostamo@gmail.com>
2014-11-17 00:53:10 +0200

    asd

---
+++ calendar.php
@@ -0,0 +1,3 @@
+<?php
+	$url = 'https://www.tut.fi/calendarservice/calendarfeed/events/131838/fi/1069633e65c64df69f2a68aa8252a17971bb94da.ics';
+	echo file_get_contents($url);
---
+++ js/config.js
@@ -0,0 +1,24 @@
+// for navigator language
+var lang = window.navigator.language;
+// you can change the language
+//var lang = 'en';
+
+//change weather params here:
+//units: metric or imperial
+var weatherParams = {
+    'q':'Tampere,Finland',
+    'units':'metric',
+    'lang':lang
+};
+
+//var feed = 'http://www.hs.fi/uutiset/rss/';
+var feed = 'http://mix.chimpfeedr.com/0ab83-hspriority';
+//var feed = 'http://www.nu.nl/feeds/rss/opmerkelijk.rss';
+//var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+
+// compliments:
+var compliments = [
+            'Näytät hyvältä!',
+            'Vau!',
+            'Nam!',
+        ];
